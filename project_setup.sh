5ea79abc60d150c4facca03e64a5a4bc32dcf129
Reynald Pader <rey.pader@paymaya.com>
2017-09-07 13:46:38 +0800

    Initial commit.

---
+++ project_setup.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+command -v gcloud >/dev/null 2>&1 || { echo >&2 "'gcloud' must be installed and is in PATH."; exit 1; }
+if [ -z "$VIRTUAL_ENV" ];
+then
+    echo "Must have an active VIRTUAL_ENV"
+    exit 1;
+fi
+gcloud_path=$(which gcloud)
+gcloud_home=${path%/bin/gcloud}
+
+echo "Found Google Cloud SDK home directory at ${gcloud_home}"
+
+echo "Creating symbolic link of Google Cloud libraries for Python into Virtual Environment site-packages (${VIRTUAL_ENV})"
+ln -s $gcloud_home/platform/google_appengine/google $VIRTUAL_ENV/lib/python2.7/site-packages/google
